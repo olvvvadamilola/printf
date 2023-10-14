85257984220d560f19e842a721099a2cf53252ce
olvvvadamilola <destric3@gmail.com>
2023-10-14 20:56:12 +0100

    created _putchar.c  and started main.h file

---
+++ _putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _putchar - function to print a character
+ * @_o: character to print
+ *
+ * Return: 0
+ */
+int _putchar(char _o)
+{
+	write(1, &_o, 1);
+	return (1);
+}
---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdarg.h>
+
+#define buffer_size 1024
+
+int _putchar(char _o);
+
+#endif
