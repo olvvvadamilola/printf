492e12bcbbb8f56c2095cd4d8af9ec887a32ea62
olvvvadamilola <destric3@gmail.com>
2023-10-14 21:07:22 +0100

    updated main.h to include struct _lst and typedef

--- main.h
+++ main.h
@@ -7,6 +7,22 @@
 
 #define buffer_size 1024
 
+/**
+ * _lst - structure to hold a list of functions
+ * @_char: character to match
+ * @_funct: function to call
+ * Return: 0
+ * =======
+ * _lst_array - array of _lst structures(renamed data type)
+*/
+struct _lst
+{
+    char _char;
+    int (*_funct)(va_list);
+};
+typedef struct _lst _lst_array;
+
+
 int _putchar(char _o);
 
 #endif
