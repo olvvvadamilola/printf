844861caeeef9d2658da297dcd80a15259a5db1a
olvvvadamilola <destric3@gmail.com>
2023-10-15 02:10:21 +0100

    made changes to c_funct

--- c_funct.c
+++ c_funct.c
@@ -3,11 +3,13 @@
 /**
  * c_funct - function to print a string to stdout
  * @catalog: va_list of arguments
- * Return: 0
+ * Return: 1
  */
 int c_funct(va_list catalog)
 {
-	int _c = va_arg(catalog, int);
+	char _c;
 
-	return (_putchar(_c));
+	_c = va_arg(catalog, int);
+	(_putchar(_c));
+	return (1);
 }
