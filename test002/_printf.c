d78e769152d3626b2caec0e42b694c4a170f656b
olvvvadamilola <destric3@gmail.com>
2023-10-16 20:55:51 +0100

    reverted changes

--- test002/_printf.c
+++ test002/_printf.c
@@ -4,12 +4,10 @@
  * @format: format string
  * Return: number of characters printed.
  */
-int _printf(const char *format, ...);
 int _printf(const char *format, ...)
 {
 	int tally = 0;
 	const char *frmt;
-	int up_low;
 	va_list catalog;
 
 	if (format == NULL)
@@ -26,12 +24,7 @@ int _printf(const char *format, ...)
 		if (*frmt == '%')
 		{
 			frmt++;
-			if (*frmt == 'x' || *frmt == 'X')
-			{
-				up_low = (*frmt == 'X') ? 1 : 0;
-				tally += hexa16(catalog, up_low);
-			}
-			else if (_search(*frmt))
+			if (_search(*frmt))
 				tally += select_funct(*frmt)(catalog);
 			else if (*frmt == '%')
 			{
